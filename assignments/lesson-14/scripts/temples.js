cfc42a2ca1a208362147e9b09b35584bb86502cd
rifedesma <drlisonbee@gmail.com>
2019-07-23 01:43:16 -0600

    changed name to just san antonio

--- assignments/lesson-14/scripts/temples.js
+++ assignments/lesson-14/scripts/temples.js
@@ -27,38 +27,37 @@ function populateHeader(jsonObj) {
 function showInfo(jsonObj) {
     var temples = jsonObj['temples'];
 
-    for (var i = 0; i < temples.length; i++)
-        if (i == 1 || i == 5 || i == 6) {
-            var myArticle = document.createElement('article');
-            myArticle.id = 'templeArticle';
-            var myH2 = document.createElement('h2');
-            var myPara1 = document.createElement('p');
-            var myPara2 = document.createElement('p');
-            var myPara3 = document.createElement('p');
-            var myPara4 = document.createElement('p');
-            var imgPath = 'images/';
-            imgPath += temples[i].name.toLowerCase().replace(' ', '');
-            imgPath += '.jpg';
-            var newImg = document.createElement('IMG');
-            newImg.setAttribute("src", imgPath);
-            console.log(imgPath);
+    for (var i = 0; i < temples.length; i++) {
+        var myArticle = document.createElement('article');
+        myArticle.id = 'templeArticle';
+        var myH2 = document.createElement('h2');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
+        var imgPath = 'images/';
+        imgPath += temples[i].name.toLowerCase().replace(' ', '');
+        imgPath += '.jpg';
+        var newImg = document.createElement('IMG');
+        newImg.setAttribute("src", imgPath);
+        console.log(imgPath);
 
 
 
 
-            myH2.textContent = temples[i].name + ' ';
-            myPara1.textContent = 'Date Announced: ' + temples[i].announced + ' ';
-            myPara2.textContent = 'Date Dedicated: ' + temples[i].dedicated;
-            myPara3.textContent = 'Temple Closures 2019: ' + temples[i].TempleClosures;
-            newImg.imgPath = imgPath;
+        myH2.textContent = temples[i].name + ' ';
+        myPara1.textContent = 'Date Announced: ' + temples[i].announced + ' ';
+        myPara2.textContent = 'Date Dedicated: ' + temples[i].dedicated;
+        myPara3.textContent = 'Temple Closures 2019: ' + temples[i].TempleClosures;
+        newImg.imgPath = imgPath;
 
 
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myArticle.appendChild(myPara3);
-            myArticle.appendChild(myPara4);
-            myArticle.appendChild(newImg);
-            section.appendChild(myArticle);
-        }
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myPara4);
+        myArticle.appendChild(newImg);
+        section.appendChild(myArticle);
+    }
 }
